d680abf61a6cfb1604e9efa73a0033fc7c32b59a
SimonCamilo <109754731+SimonCamilo@users.noreply.github.com>
2024-10-03 19:28:21 -0300

    att das aulas
    estou aprendendo sobre git hub

---
+++ matematica/calculadora.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def soma(x,y):
+    """
+    param x = primeiro numero
+    param y = segundo numero
+    faze a soma entre x e y
+    ex: soma(1,2)
+    = 3
+    """
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mult(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
---
+++ matematica/geometria.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import math
+def areaCirculo(r):
+    return math.pi*math.pow (r,2)
+    
+
